f81c6324b71f60a31d290500c815fba88938324a
Tessie Baumann <tabaumann@email.wm.edu>
2020-10-25 14:35:56 -0400

    Added PlayManuallyActivity class with appearance set

---
+++ app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/PlayManuallyActivity.java
@@ -0,0 +1,21 @@
+package edu.wm.cs.cs301.TessieBaumann.gui;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.TessieBaumann.R;
+
+public class PlayManuallyActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_manually_activity);
+
+
+    }
+}
