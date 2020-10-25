964f227fb590435e47d67a8c57a223d3180940d4
Tessie Baumann <tabaumann@email.wm.edu>
2020-10-25 15:32:04 -0400

    Added PlayAnimationActivity class with appearance set

---
+++ app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/PlayAnimationActivity.java
@@ -0,0 +1,18 @@
+package edu.wm.cs.cs301.TessieBaumann.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.TessieBaumann.R;
+
+public class PlayAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_animation_activity);
+
+
+    }
+}
