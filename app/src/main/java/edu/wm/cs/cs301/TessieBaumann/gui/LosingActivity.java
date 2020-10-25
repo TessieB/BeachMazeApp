4236ae066a3e17c32a59acb4ba56174f1f598270
Tessie Baumann <tabaumann@email.wm.edu>
2020-10-25 15:45:21 -0400

    Added WinningActivity class with appearance set

---
+++ app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/LosingActivity.java
@@ -0,0 +1,4 @@
+package edu.wm.cs.cs301.TessieBaumann.gui;
+
+public class LosingActivity {
+}
---
+++ app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/WinningActivity.java
@@ -0,0 +1,19 @@
+package edu.wm.cs.cs301.TessieBaumann.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.TessieBaumann.R;
+
+public class WinningActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.winning_activity);
+
+
+    }
+}
