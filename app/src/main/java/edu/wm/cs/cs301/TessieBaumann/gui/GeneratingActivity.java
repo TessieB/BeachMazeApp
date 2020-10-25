610940f80df47f25aef7746a194590c85c10fdca
Tessie Baumann <tabaumann@email.wm.edu>
2020-10-25 12:33:57 -0400

    Added Generating Activity class with appearance set

--- app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/GeneratingActivity.java
@@ -1,4 +1,26 @@
 package edu.wm.cs.cs301.TessieBaumann.gui;
 
-public class GeneratingActivity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.TessieBaumann.R;
+
+public class GeneratingActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generating_activity);
+        Spinner builderSpinner = (Spinner) findViewById(R.id.sensorspinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sensor, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        builderSpinner.setAdapter(adapter);
+
+
+
+    }
 }
