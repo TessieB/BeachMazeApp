ed2984f56e630ae9940710576f688c149c1baf99
Tessie Baumann <tabaumann@email.wm.edu>
2020-10-25 15:53:22 -0400

    Added LosingActivity class with appearance set

--- app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/LosingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/TessieBaumann/gui/LosingActivity.java
@@ -1,4 +1,18 @@
 package edu.wm.cs.cs301.TessieBaumann.gui;
 
-public class LosingActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.TessieBaumann.R;
+
+public class LosingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.losing_activity);
+
+
+    }
 }
